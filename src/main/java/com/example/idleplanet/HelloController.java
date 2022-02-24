ecb1fbdf614716b80c3fa3652ef39a68df8714d9
Jo <johannes.stind@htl-saalfelden.at>
2022-02-24 14:25:28 +0100

    test1

--- src/main/java/com/example/idleplanet/HelloController.java
+++ src/main/java/com/example/idleplanet/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.idleplanet;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    @FXML
+    Button button1;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
