16a3f9da42d6433fea7537fc1871803142292a32
Jo <johannes.stind@htl-saalfelden.at>
2022-02-24 14:22:15 +0100

    Revert "test1"
    This reverts commit c4281e3ee56de9c0ad492703e8aaedf5c8592f16.

--- src/main/java/com/example/idleplanet/HelloController.java
+++ src/main/java/com/example/idleplanet/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.idleplanet;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -12,6 +11,4 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-    @FXML
-    Button button1;
 }
