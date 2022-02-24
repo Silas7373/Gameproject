182e48836c5dfe65ccb32c794c77e0da413db52b
Silas <silas.kocher@htl-saalfelden.at>
2022-02-24 14:09:05 +0100

    Revert "initial2"
    This reverts commit afac067d03be6f9e8ded2d8a7dd9b9ecf3e48f58.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.idleplanet {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.idleplanet to javafx.fxml;
-    exports com.example.idleplanet;
-}
