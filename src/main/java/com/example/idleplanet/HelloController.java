fd9a435b2d53da1bbaf16019560f6cb17b65d5fc
Silas <silas.kocher@htl-saalfelden.at>
2022-03-03 15:00:52 +0100

    Timer

--- src/main/java/com/example/idleplanet/HelloController.java
+++ src/main/java/com/example/idleplanet/HelloController.java
@@ -5,7 +5,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
-public class HelloController {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Serializable;
+import javax.swing.Timer;
+
+
+public class HelloController
+        implements Serializable {
     @FXML
     private Label welcomeText;
     @FXML
@@ -16,12 +23,49 @@ public class HelloController {
     Label points;
 
 
+    Timer timer;
 
     Currency currency = new Currency(0);
-    public void onButtonClick(){
-    currency.setMoney(currency.money + currency.getPlanetstage());
-    points.setText(currency.getMoney());
+    int currencyCounter = 0;
+    int geld = currency.getMoneyint();
+
+    public void onButtonClick() {
+        currency.setMoney(currency.money + currency.getPlanetstage());
+        points.setText(currency.getMoney());
+
     }
 
+    public void Start() {
+        setTimer();
+        timer.start();
 
+    }
+
+    /*public void Automatic(){
+        for(int i = 0; 1<3;i++){
+            currency.setMoney(currency.getMoneyint()+currency.getPlanetstage());
+            points.setText(currency.getMoney());
+
+        }
+    }*/
+    public void setTimer() {
+        currencyCounter = currency.getMoneyint();
+
+
+        timer = new Timer(1000, new ActionListener() {
+
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currencyCounter += 1;
+                points.setText(String.valueOf(currencyCounter));
+
+            }
+        });
+
+
+    }
+
+    public void Automatic(javafx.event.ActionEvent actionEvent) {
+    }
 }
