9f6f827c1ab5f8da9b0385293e7d8cabafe242e2
Jo <johannes.stind@htl-saalfelden.at>
2022-06-02 14:55:05 +0200

    upgrades

--- src/main/java/com/example/idleplanet/MoreCpC.java
+++ src/main/java/com/example/idleplanet/MoreCpC.java
@@ -5,7 +5,7 @@ public class MoreCpC extends Upgrade{
 
     MoreCpC(int setUpgradeLvl){
         this.upgradeLvL = setUpgradeLvl;
-        cost = 10 * 1.2 * (double) upgradeLvL;
+        cost = 10 *Math.pow(upgradeLvL,3) * 1.2;
         plusMoney = 1;
     }
 
